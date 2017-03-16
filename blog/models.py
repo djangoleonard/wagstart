ea6c1c8335038eff110ca256a452ac00093550ce
Leonard <aktasm2005@hotmail.com>
2017-03-16 10:57:21 +0300

    Blog Index Page Added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from __future__ import absolute_import, unicode_literals
 
-# Create your models here.
+# from django.db import models
+
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
